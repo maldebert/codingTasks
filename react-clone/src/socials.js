10ebf9c3280d8a3229f79270489e47f5ad492518
Marie <ma-aldebert@hotmail.fr>
2024-07-07 19:35:44 +0200

    Updated social links

--- react-clone/src/socials.js
+++ react-clone/src/socials.js
@@ -4,11 +4,24 @@ const Socials = () => {
   return (
     <div>
       <div className="socials">
-        <img src="https://www.iconpacks.net/icons/2/free-instagram-logo-icon-3497-thumb.png"></img>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Facebook%2BIcon%2BBlack.png"></img>
-        <img src="https://seeklogo.com/images/L/linkedin-logo-A6EF048412-seeklogo.com.png"></img>
-        <img src="https://cdn-icons-png.flaticon.com/512/60/60580.png"></img>
-        <img src="https://static.vecteezy.com/system/resources/thumbnails/023/986/739/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"></img>
+        <a href="https://www.instagram.com/maisonmirabeau/" target="_blank">
+          <img src="https://www.iconpacks.net/icons/2/free-instagram-logo-icon-3497-thumb.png"></img>
+        </a>
+        <a href="https://www.facebook.com/maisonmirabeau" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Facebook%2BIcon%2BBlack.png"></img>
+        </a>
+        <a
+          href="https://fr.linkedin.com/company/maison-mirabeau"
+          target="_blank"
+        >
+          <img src="https://seeklogo.com/images/L/linkedin-logo-A6EF048412-seeklogo.com.png"></img>
+        </a>
+        <a href="https://twitter.com/maisonmirabeau" target="_blank">
+          <img src="https://cdn-icons-png.flaticon.com/512/60/60580.png"></img>
+        </a>
+        <a href="https://www.youtube.com/@maisonmirabeau" target="_blank">
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/023/986/739/small_2x/youtube-logo-youtube-logo-transparent-youtube-icon-transparent-free-free-png.png"></img>
+        </a>
       </div>
       <p className="precautions">
         Do not share or forward to people under legal drinking age. Enjoy
